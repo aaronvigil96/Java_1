320922eaf2e6a46f831fee418d681efef18685c7
Aaron Vigil <aaronvigil96@gmail.com>
2023-03-08 16:57:05 -0300

    Subiendo mi primera aplicacion de Java

---
+++ src/primeraplicacion/PrimerAplicacion.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primeraplicacion;
+
+/**
+ *
+ * @author Aaron
+ */
+public class PrimerAplicacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       byte edad;
+       String nombre;
+       String apellido;
+       boolean secundario;
+       edad = 26;
+       nombre = "Aaron";
+       apellido = "Vigil";
+       secundario = true;
+       
+       System.out.println("Mi nombre es " + nombre + " " + apellido + " tengo " + edad + " años");
+    }
+    
+}
